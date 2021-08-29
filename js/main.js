1e761a5dfd95785bb8627504c75882ceb16a30bf
OlgaTiempo <olenka.zhigareva@mail.ru>
2021-08-29 21:27:27 +0300

    lesson-2

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var user="John Doe";
+console.log(user);
+var student="Olga";
+console.log(student);
+var user="student";
+console.log(user);
+let test=1;
+console.log(test);
+++test;
+console.log(test);
+console.log(test+1);
+console.log(test);
+--test;
+console.log(test);
+if (test)
+{
+    console.log(true);
+} else {
+    console.log(false)
+}
+console.log(test);
+
+let result =1;
+let arr =[2,3,5,8];
+for (let i =0; i<arr.length; i++) {
+ result *= arr[i];
+}
+console.log(result);
+
+let arrr =[2,5,8,15,0,6,20,3];
+for (let i =0; i<arrr.length; i++) {
+    if (arrr[i]<10 & arrr[i]>5) {
+        console.log (arrr[i]);
+    }
+}
+
+let ars =[2,5,8,15,0,6,20,3];
+for (let i =0; i<ars.length; i++) {
+    if (ars[i]%2===0) {
+        console.log (ars[i]);
+    }
+}
